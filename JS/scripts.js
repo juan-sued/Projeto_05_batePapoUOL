87ae2429976ab970afb26e4a6f5783cc45e70748
Juan <juansued19@gmail.com>
2022-04-16 17:01:06 -0300

    Versão recebendo mensagens

--- JS/scripts.js
+++ JS/scripts.js
@@ -5,11 +5,11 @@ const loginPage = document.querySelector(".loginPage");
 const mainPage = document.querySelector(".mainPage");
 
 const urlStatusAPI = "https://mock-api.driven.com.br/api/v6/uol/status";
-
 const urlPaticipantsAPI =
   "https://mock-api.driven.com.br/api/v6/uol/participants";
-
 const urlMessagesAPI = "https://mock-api.driven.com.br/api/v6/uol/messages";
+
+let responseMessages = [];
 
 function login() {
   console.log("clicou");
@@ -25,11 +25,11 @@ function login() {
     }, 3000);
     userLogin.name = inputName;
   } else {
-    serverContact(userLogin);
+    serverContactPost(userLogin);
   }
 }
 
-function serverContact(element) {
+function serverContactPost(element) {
   console.log(userLogin.name);
   const promisse = axios.post(
     "https://mock-api.driven.com.br/api/v6/uol/participants",
@@ -47,27 +47,55 @@ function error(error) {
     setTimeout(function () {
       window.location.reload(true);
     }, 3000);
+  } else {
+    console.log("(dentro da function erro)erro numero" + error.response.stats);
   }
 }
 
 function chatOn() {
   loginPage.classList.add("hidden");
   mainPage.classList.remove("hidden");
-  console.log(userLogin.name);
+  console.log(chatOn);
+
+  serverContactGet();
+
   setInterval(verificaOnline, 4000);
-  mensagemLogin();
-  newMensagem();
 }
+
 function verificaOnline() {
   const promisse = axios.post(urlStatusAPI, userLogin);
   promisse.then(console.log("Verificado que está online"));
 }
 
+/*
+=== Forma da mensagem === Response
+
+
+from: "jessé"
+text: "whip"
+time: "05:40:39"
+to: "Todos"
+type: "message"
+
 function mensagemLogin() {
-  const ul = document.querySelector("ul");
-  ul.innerHTML += `<li class='messageStatus'><strong>${userLogin.name} &nbsp</strong> entrou na sala...</li>`;
 }
-function newMensagem() {
+*/
+
+function serverContactGet() {
   const promisse = axios.get(urlMessagesAPI);
   console.log(promisse);
+  promisse.then(loadMessages);
+  promisse.catch(error);
+}
+function loadMessages(response) {
+  responseMessages = response.data;
+  console.log(responseMessages);
+  renderMessages(responseMessages);
+}
+
+function renderMessages(response) {
+  const ul = document.querySelector("ul");
+  for (let i = 0; i < responseMessages.length; i++) {
+    ul.innerHTML += `<li class='messageToAll'><span class='timeMessage'> (${responseMessages[i].time})&nbsp </span><span class='contentMessage'><strong> ${responseMessages[i].from}</strong>&nbsppara<strong>&nbsp${responseMessages[i].to}</strong>: ${responseMessages[i].text}</span></li>`;
+  }
 }
